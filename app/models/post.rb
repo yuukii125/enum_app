e6b2e4474550c76e0200176e64e1d5ef52516484
yuukii125 <y.kipro125@gmail.com>
2021-08-10 23:45:00 +0900

    enumの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
   validates :genre, presence: true
   validates :content, presence: true
+
+  enum genre: {
+    others: 0,
+    html: 1,
+    css: 2,
+    js: 3,
+    ruby: 4,
+    rails: 5
+  }
 end
