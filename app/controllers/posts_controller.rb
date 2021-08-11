dffaef072fdeea9166f3ac554c73375698632461
yuukii125 <y.kipro125@gmail.com>
2021-08-11 17:45:13 +0900

    新規投稿ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,21 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_url
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:genre, :content)
   end
 end
